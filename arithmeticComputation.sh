89d9116c65d2e1ff8b854129fbb92c851b76017e
unknown <ariprasath3@gmail.com>
2021-06-09 07:57:22 +0530

    added: take 3 inputs from the user

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,1 +1,4 @@
-#Arithmetic computation and sorting. This problem computes different arithmetic expressions and sorts the results.
+#!/bin/bash -x
+read -p "Enter the first number" a;
+read -p "Enter the second number" b;
+read -p "Enter the third number" c;
